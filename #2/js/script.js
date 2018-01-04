0bee27bf36c8a8a83e00c3c9538872f239b31de3
luschenko <you@example.com>
2018-01-04 08:10:38 +0200

    add lesson 2

--- #2/js/script.js
+++ #2/js/script.js
@@ -1,5 +1,21 @@
-// Идентификатор
-// Регистр!!!!!!!
-function summa(){
-	
-}
+   // x
+/* 
+* count
+* getElementById
+* setCount
+*/
+//getX;
+//new
+a = 'Ivanov';
+fio = '';
+polzovatel, pokupatel;
+customerName
+1customer
+customer1
+customer_1
+customer_name
+$customer_name
+_customerName
+//temp - временная переменная
+a, b, c, temp, temp1
+rectWidth, rectangularWidth, rect_width, rect_w
