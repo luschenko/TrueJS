26686ed53f3ddf74889cadb5936ffccf50b2a946
luschenko <you@example.com>
2018-01-09 07:32:40 +0200

    lesson 3

---
+++ #3/js/script.js
@@ -0,0 +1,30 @@
+// console.log(4/6);
+// var x;
+// var y;
+// y = 3;
+// x = 5 +y; //8
+// x = 'Hello'; //Hello
+// x = 6;
+// x = x + 4;
+// x +=4;
+// var x;
+// var x=9;
+// console.log (x);
+
+// let y = 12;
+// var y = 5;
+// if (1) {
+// 	y = 7;
+// 	console.log(y);
+// }
+// console.log(y);
+
+// let z = 5; // {}
+// if (1) {
+// 	z = 7;
+// 	console.log(z);
+// }
+// console.log(z);
+
+const A = 9;
+console.log(A);
