929cee4e462e4bd5bc28abad5ad3219715d2dbf0
Ilya Ivanouski <domovikx@gmail.com>
2019-06-10 08:57:08 +0300

    Update script.js

--- #05/#5/js/script.js
+++ #05/#5/js/script.js
@@ -3,11 +3,12 @@
 // hoisting
 
 // ES 5
-// console.log(a);
+// console.log(a); // undefined 
 // var a=7;
-// console.log(a);
+// console.log(a); // 7 
 // a = 5;
-// console.log(a);
+// console.log(a); // 5
 
-console.log(b);
+// ES 6
+console.log(b); // b is not defined
 let b = 7;
